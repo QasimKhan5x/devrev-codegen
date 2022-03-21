40e68ecc0fbdc381047b147703b5e6064e9c8a23
Qasim Khan <straightouttadetroit.qk@gmail.com>
2022-03-21 17:04:28 +0500

    Update code_gen.py

--- code_gen.py
+++ code_gen.py
@@ -7,15 +7,29 @@ load_dotenv()  # take environment variables from .env.
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-def get_code(prompt, temperature=0, max_tokens=64, frequency_penalty=0, best_of=1):
+def get_code(prompt, **kwargs):
+    '''
+    Useful parameters
+        - temperature/top_p
+        - best_of
+        - max_tokens
+        - frequency_penalty
+        - presence_penalty
+    '''
+    default_kwargs = {
+        "top_p": 1.0,
+        "max_tokens": 256,
+    }
+    # set default value if not present in kwarfs
+    for k, v in default_kwargs.items():
+        if k not in kwargs:
+            kwargs[k] = v
+    # wrap in docstring
     prompt = f'"""{prompt}"""'
     result = openai.Completion.create(
         engine="code-davinci-001",
         prompt=prompt,
-        temperature=temperature,
-        max_tokens=max_tokens,
-        frequency_penalty=frequency_penalty,
-        best_of=best_of
+        **kwargs
         )
     code = result["choices"][0]["text"].split("\n")
     code = list(filter(lambda x: x != '', code))
