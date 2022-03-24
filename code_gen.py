5add1de8e3cf4f56bfb6b63fa74d7241b136279b
QasimKhan5x <straightouttadetroit.qk@gmail.com>
2022-03-24 13:43:15 +0500

    update get_code

--- code_gen.py
+++ code_gen.py
@@ -1,22 +1,47 @@
 import os
-from dotenv import load_dotenv
 
 import openai
+from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-def get_code(prompt, temperature=0, max_tokens=64, frequency_penalty=0, best_of=1):
-    prompt = f'"""{prompt}"""'
+
+def get_code(prompt, **kwargs):
+    '''
+    Useful parameters
+        - temperature/top_p
+        - best_of
+        - max_tokens
+        - frequency_penalty
+        - presence_penalty
+        - stop
+        - n
+    '''
+    default_kwargs = {
+        "top_p": 1.0,
+        "max_tokens": 256,
+    }
+    # set default value if not present in kwargs
+    for k, v in default_kwargs.items():
+        if k not in kwargs:
+            kwargs[k] = v
     result = openai.Completion.create(
         engine="code-davinci-001",
         prompt=prompt,
-        temperature=temperature,
-        max_tokens=max_tokens,
-        frequency_penalty=frequency_penalty,
-        best_of=best_of
-        )
-    code = result["choices"][0]["text"].split("\n")
-    code = list(filter(lambda x: x != '', code))
-    return code
+        **kwargs
+    )
+    if 'n' in kwargs:
+        codes = []
+        for code in result["choices"]:
+            code = code["text"].split("\n")
+            code = list(filter(lambda x: x != '', code))
+            code = "\n".join(code)
+            codes.append(code)
+        return codes
+    else:
+        code = result["choices"][0]["text"].split("\n")
+        code = list(filter(lambda x: x != '', code))
+        code = "\n".join(code)
+        return code
